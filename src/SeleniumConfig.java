d2ec1bd0d939a91175889fc0d32d446129480afe
MarcoCangela <Mcangelaoo@gmail.com>
2023-08-21 19:24:22 +0200

    Removing Boilerplate code

--- src/SeleniumConfig.java
+++ src/SeleniumConfig.java
@@ -3,6 +3,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumConfig {
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(); //Launching Chrome Driver
     }
 }
