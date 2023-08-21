9dc6a01b09e653d984b7095000b750997a5b2985
MarcoCangela <Mcangelaoo@gmail.com>
2023-08-21 19:38:14 +0200

    Adding JARs folder to gitignore and performing remaining configs to webdriver

--- src/SeleniumConfig.java
+++ src/SeleniumConfig.java
@@ -1,8 +1,18 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumConfig {
     public static void main(String[] args) {
+
+        System.setProperty("webdricer.chrome.driver","/../JARs"); //Adding the Chromedriver
+
         WebDriver driver = new ChromeDriver(); //Launching Chrome Driver
+
+        /**
+         * Using the Webdriver to test a website
+         */
+        driver.get("https://wikipedia.com"); //Navigating to the website to test
+        driver.findElement(By.id("js-link-box-en")).click(); //Finding and clicking the element to test on the website/page to test 
     }
 }
